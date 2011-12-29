e52e00547343c0dc9ae4e4c40d194b82ec5a8e43
andreszeballosjuanico@gmail.com <andreszeballosjuanico@gmail.com@c902db13-a5c2-d13c-9785-d11b84980aaa>
2011-12-29 17:10:10 +0000

    ...

---
+++ trunk/bugtrackerofmagic/src/magic/Juego.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package magic;
+
+/**
+ *
+ * @author Andres
+ */
+public class Juego {
+    private int id;
+    private Jugador jugador1;
+    private Jugador jugador2;
+    private Pila pila;
+}
