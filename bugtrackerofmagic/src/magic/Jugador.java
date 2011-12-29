efde7d7daad4668a9e995db9989704f769f1ce71
andreszeballosjuanico@gmail.com <andreszeballosjuanico@gmail.com@c902db13-a5c2-d13c-9785-d11b84980aaa>
2011-12-29 17:10:10 +0000

    ...

---
+++ bugtrackerofmagic/src/magic/Jugador.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package magic;
+
+import java.util.List;
+
+/**
+ *
+ * @author Andres
+ */
+public class Jugador {
+    List<ICarta> mano;
+    List<ICarta> biblioteca;
+    List<ICarta> cementerio;
+    List<ICarta> en_juego;
+    List<ICarta> removidas;
+    
+}
